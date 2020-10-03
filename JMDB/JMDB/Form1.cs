79f5f6f5ecae78a6da6668fdbf6ddbe906f9216c
GrindhouseJames <72271325+GrindhouseJames@users.noreply.github.com>
2020-10-03 16:15:00 +0100

    Add files via upload

---
+++ JMDB/JMDB/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace JMDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TestButton(object sender, EventArgs e) //This is for connecting to the SQL server
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=DESKTOP-IEGK5H0;Initial Catalog=JMDB;User ID=james;Password=1234";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+            MessageBox.Show("Connection Open!");
+            cnn.Close();
+        }
+
+        private void TitleTextbox(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GenreTextbox(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YearTextbox(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, KeyEventArgs e) //This is the submit button
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                MessageBox.Show("Film: " + textBox1.Text + "\n" + "Genre: " + textBox2.Text + "\n" + "Year: " + textBox3.Text);
+            }
+        }
+
+    }
+}
